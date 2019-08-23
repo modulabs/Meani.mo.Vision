f8e65e2348bc28cf094fc5d8541ecf59eaea4c8a
visionNoob <insurgent92@gmail.com>
2019-08-23 22:38:33 +0900

    Fix PEP8 issue
    Line 3:1: E302 expected 2 blank lines, found 1
    Line 5:22: E231 missing whitespace after ','
    Line 5:32: E231 missing whitespace after ','
    Line 5:43: E231 missing whitespace after ','
    Line 5:53: E231 missing whitespace after ','
    Line 17:19: E203 whitespace before ':'
    Line 20:28: E231 missing whitespace after ','
    Line 24:80: E501 line too long (119 > 79 characters)
    Line 24:114: E231 missing whitespace after ','
    Line 25:80: E501 line too long (119 > 79 characters)
    Line 25:114: E231 missing whitespace after ','
    Line 27:40: E231 missing whitespace after ','

--- featureDetector.py
+++ featureDetector.py
@@ -1,8 +1,9 @@
 import cv2
 
+
 class FeatureDetector:
 
-    def __init__(self,drone_img,algorithm1,lidar_img,algorithm2):
+    def __init__(self, drone_img, algorithm1, lidar_img, algorithm2):
 
         self._init_detector_funcs()
 
@@ -14,17 +15,19 @@ class FeatureDetector:
 
     def _init_detector_funcs(self):
         self._detectors = {
-            "SIFT" : cv2.xfeatures2d.SIFT_create
+            "SIFT": cv2.xfeatures2d.SIFT_create
         }
 
-    def _load_detector(self,algorithm):
+    def _load_detector(self, algorithm):
         return self._detectors[algorithm]()
 
     def compute_features_and_descriptor(self):
-        self.drone_img_features, self.drone_img_descs = self._drone_img_detector.detectAndCompute(self._drone_img,None)
-        self.lidar_img_features, self.lidar_img_descs = self._lidar_img_detector.detectAndCompute(self._lidar_img,None)
+        self.drone_img_features, self.drone_img_descs = self._drone_img_detector.detectAndCompute(
+            self._drone_img, None)
+        self.lidar_img_features, self.lidar_img_descs = self._lidar_img_detector.detectAndCompute(
+            self._lidar_img, None)
 
-    def get_feature_and_descriptor(self,img_type):
+    def get_feature_and_descriptor(self, img_type):
         if img_type == "drone":
             return self.drone_img_features, self.drone_img_descs
         elif img_type == "lidar":
