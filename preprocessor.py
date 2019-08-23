3c4a199ab2591a109e3ad21dbd85c182972cb7d1
kooock <koock1994@gmail.com>
2019-08-23 23:00:39 +0900

    base code initial commit (#1)
    * base code initial commit
    
    * Fix PEP8 issue
    
    Line 3:1: E302 expected 2 blank lines, found 1
    Line 5:22: E231 missing whitespace after ','
    Line 5:32: E231 missing whitespace after ','
    Line 5:43: E231 missing whitespace after ','
    Line 5:53: E231 missing whitespace after ','
    Line 17:19: E203 whitespace before ':'
    Line 20:28: E231 missing whitespace after ','
    Line 24:80: E501 line too long (119 > 79 characters)
    Line 24:114: E231 missing whitespace after ','
    Line 25:80: E501 line too long (119 > 79 characters)
    Line 25:114: E231 missing whitespace after ','
    Line 27:40: E231 missing whitespace after ','
    
    * Fix PEP 8 issues
    
    * Fix PEP8 issues
    
    * Fix PEP8 issues
    
    * Fix PEP8
    
    * Delete "\"

---
+++ preprocessor.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+class Preprocessor:
+
+    def __init__(self, drone_img, lidar_img):
+        self.drone_img = drone_img
+        self.lidar_img = lidar_img
+
+        self.processed_drone_img = None
+        self.processed_lidar_img = None
+
+    def preprocessing(self):
+        self.processed_drone_img = self._drone_img_preprocessing()
+        self.processed_lidar_img = self._lidar_img_preprocessing()
+
+    def _drone_img_preprocessing(self):
+        # TODO
+        return cv2.GaussianBlur(
+            cv2.cvtColor(
+                self.drone_img, cv2.COLOR_BGR2GRAY), (5, 5), 0)
+
+    def _lidar_img_preprocessing(self):
+        # TODO
+        return cv2.medianBlur(self.lidar_img, 5)
+
+    def get_processed_imgs(self):
+        return self.processed_drone_img, self.processed_lidar_img
