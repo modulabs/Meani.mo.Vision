af63fe820cf548b5c0b2410ba8f8deedce8ee555
visionNoob <insurgent92@gmail.com>
2019-08-23 22:58:42 +0900

    Delete "\"

--- matcher.py
+++ matcher.py
@@ -58,7 +58,7 @@ class Matcher:
     def draw_matches(self, drone_img, lidar_img, file_name):
         matchesMask = self._status.ravel().tolist()
         height, width = drone_img.shape
-        pts = np.float32([[0, 0], [0, height - 1], [width - 1,\
+        pts = np.float32([[0, 0], [0, height - 1], [width - 1,
                                                     height - 1], [width - 1, 0]]).reshape(-1, 1, 2)
 
         dst = cv2.perspectiveTransform(pts, self._homography)
